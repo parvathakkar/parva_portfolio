01695299fbfa076beb8147dd8f4dcabb4df20118
Parva <parvathakkar12@gmail.com>
2025-08-18 14:17:00 -0400

    add experience section

--- src/pages/Experience/experience.jsx
+++ src/pages/Experience/experience.jsx
@@ -1,6 +1,49 @@
 import react from "react";
 import "./experience.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import HomeIcon from "@mui/icons-material/Home";
 
 export default function Experience() {
-  return <div>Experience</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="exp-container">
+      <h1>My Experience</h1>
+      <div className="work-container">
+        <div className="work-title">
+          <img src="/ec.png" alt="Canada Flag" className="canada-img" />
+          <p>Software Developer</p>
+          <p>Environment And Climate Change Canada</p>
+          <p>May 2023 - August 2024</p>
+        </div>
+        <div className="work-duties">
+          <ul>
+            <li>
+              Contributed to the planning, design, and implementation of Java
+              Components that improved weather forecasting accuracy, optimized
+              metadata services, and significantly reduced data processing times
+            </li>
+            <li>
+              Led testing efforts using Docker, automated API testing with
+              Python scripts, and created test cases to identify and fix
+              critical bugs, ensuring high product quality and stability.
+            </li>
+            <li>
+              Engineered and maintained Jenkins CI/CD pipelines to automate
+              build, test, and deployment workflows
+            </li>
+            <li>
+              Collaborated in Agile SDLC processes to streamline development and
+              reduce delivery times
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="button" onClick={() => navigate(`/`)}>
+        <Button variant="contained" endIcon={<HomeIcon />}>
+          Back Home
+        </Button>
+      </div>
+    </div>
+  );
 }
