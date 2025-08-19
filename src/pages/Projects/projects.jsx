fd3e2bd0e1cb329878e83785dacd29120fc277dc
Parva <parvathakkar12@gmail.com>
2025-08-19 14:03:36 -0400

    move project images to public and reference src

--- src/pages/Projects/projects.jsx
+++ src/pages/Projects/projects.jsx
@@ -16,16 +16,14 @@ const Projects = () => {
       title: "Sportsbook Assistant",
       description:
         "Assistant to help you find the best odds across multiple sportsbooks",
-      imageUrl:
-        "https://cdn.prod.website-files.com/64de31713f61e6b251fc08ae/650070a8f55b545bd61b0455_Sportsbooks.png",
+      src: "/bet.png",
       status: "Completed",
     },
     {
       id: 2,
       title: "Hotel/Restaurant Finder",
       description: "Find open restaurants and hotels near you",
-      imageUrl:
-        "https://techcrunch.com/wp-content/uploads/2015/08/shutterstock_237882754.jpg?resize=1200,800",
+      src: "/res.jpg",
       status: "Completed",
     },
     {
@@ -33,16 +31,14 @@ const Projects = () => {
       title: "Therapist and Patient Engagement Detector",
       description:
         "Provides visual and quantitative feedback to therapists on how engaged their patients are during sessions",
-      imageUrl:
-        "https://www.health.com/thmb/GlslebWLT9DwbeA_QCL1gh3aqkE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cbt-GettyImages-1368430888-4f6382a186b046ebbc030765d28a86c4.jpg",
+      src: "https://www.health.com/thmb/GlslebWLT9DwbeA_QCL1gh3aqkE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cbt-GettyImages-1368430888-4f6382a186b046ebbc030765d28a86c4.jpg",
       status: "Completed",
     },
     {
       id: 4,
       title: "Book Store Inventory System",
       description: "A system to manage book inventory for a bookstore",
-      imageUrl:
-        "https://image.freepik.com/free-photo/stack-books-library_42493-283.jpg",
+      src: "/books.jpeg",
       status: "Completed",
     },
     {
@@ -50,16 +46,14 @@ const Projects = () => {
       title: "Card Counter Catcher",
       description:
         "A computer vision project to detect card counters in a casino",
-      imageUrl:
-        "https://www.crownperth.com.au/getmedia/7171081e-e6fd-487a-adfc-1ade1f75edee/220815-Crown-Perth-Gaming-Blackjack-1200x800px-6.jpg",
+      src: "/cards.jpg",
       status: "In Progress",
     },
     {
       id: 6,
       title: "Spring Boot Banking System",
       description: "A banking system built with Spring Boot",
-      imageUrl:
-        "https://dashboard.thefinanser.com/wp-content/uploads/2021/09/bank.jpeg",
+      src: "/bank.jpeg",
       status: "In Progress",
     },
   ];
@@ -82,7 +76,7 @@ const Projects = () => {
               <CardMedia
                 component="img"
                 height="140"
-                image={project.imageUrl}
+                image={project.src}
                 alt={project.title}
               />
               <CardContent>
